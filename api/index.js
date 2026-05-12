c51cb291da6ec3a214eebfc427c13b73c3d4dcb5
Arbaz-Ahmad95 <arbazahmad363@gmail.com>
2026-05-13 00:25:08 +0530

    up

--- api/index.js
+++ api/index.js
@@ -1,9 +1,47 @@
 import server from '../dist/server/server.js';
 
-export const config = {
-  runtime: 'edge',
-};
+export default async function handler(req, res) {
+  const protocol = req.headers['x-forwarded-proto'] || 'https';
+  const host = req.headers['x-forwarded-host'] || req.headers.host || 'localhost';
+  const url = new URL(req.url, `${protocol}://${host}`);
 
-export default async function (request) {
-  return server.fetch(request, process.env, {});
+  const init = {
+    method: req.method,
+    headers: req.headers,
+  };
+
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    init.body = Buffer.concat(buffers);
+  }
+
+  const webRequest = new Request(url.href, init);
+
+  try {
+    const response = await server.fetch(webRequest, process.env, {});
+    
+    res.statusCode = response.status;
+    response.headers.forEach((value, key) => {
+      res.setHeader(key, value);
+    });
+
+    if (response.body) {
+      const reader = response.body.getReader();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        res.write(Buffer.from(value));
+      }
+      res.end();
+    } else {
+      res.end();
+    }
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
 }
